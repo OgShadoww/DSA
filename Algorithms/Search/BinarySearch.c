7c3aa6912e34480781fd065eb640dee5776e645b
Orest <galenzao@gmail.com>
2025-04-16 21:21:35 +0300

    Do binary search again

--- Algorithms/Search/BinarySearch.c
+++ Algorithms/Search/BinarySearch.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
-int binarySearch(int vector[], int left, int right, int n) {
-    while(left < right) {
-        int mid = (left + right) / 2;
-        if(n > vector[mid]) {
-            left = mid;
-        }
-        if(n < vector[mid]) {
-            right = mid;
-        }
-        if(n == vector[mid]) {
-            return mid;
-            break;
-        }
+int binarySearch(int arr[], int left, int right, int n) {
+  while(right > left) {
+    int mid = (right + left) / 2;
+    if(n > arr[mid]) {
+      left = mid;
     }
-    return -1;
+    if(n < arr[mid]) {
+      right = mid;
+    }
+    if(n == arr[mid]) {
+      return mid;
+    }
+  }
+
+  return -1;
 }
 
 int main() {
     int vector[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    printf("%d", binarySearch(vector, 0, 9, 0)); 
+    printf("%d", binarySearch(vector, 0, 9, 5)); 
 }
