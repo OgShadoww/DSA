7546b7fcf5f1c1f49e22e3fab8e2edffd1d7557d
Orest <galenzao@gmail.com>
2025-11-03 15:58:08 +0000

    Implementing a real dynamic array

--- include/array.h
+++ include/array.h
@@ -1,17 +1,19 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <stddef.h>
+
 typedef struct array {
   int size;
   int capacity;
   int *array;
 } array;
 
-array init_array());
-int get(array a, int i);
-void push(array &a, int v);
-void pop(array &a);
-void ensure_capacity(array &a);
-void free_vector(array &a);
+void init_array(array *a, size_t initial_cap);
+int get(array *a, int i);
+void push(array *a, int v);
+void pop(array *a);
+void ensure_capacity(array *a);
+void free_array(array *a);
 
 #endif
--- src/ds/linear/array.c
+++ src/ds/linear/array.c
@@ -1,34 +1,51 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include"../../../include/array.h"
 
-array init_array() {
-  array a;
-  a.size = 0;
-  a.capacity = 64;
-  a.array = malloc(a.capacity);
-
-  return a;
+void init_array(array *a, size_t initial_cap) {
+  a->size = 0;
+  a->capacity = 64;
+  a->array = malloc(a->capacity);
+  return;
 }
 
-void ensure_capacity(array a) {
-  if(a.capacity <= a.size) {
-    a.capacity *= 2;
-    a.array = realloc(a.array, a.capacity);
+void ensure_capacity(array *a) {
+  if(a->capacity <= a->size) {
+    a->capacity *= 2;
+    a->array = realloc(a->array, a->capacity);
   }
   return;
 }
 
-void get(array a, int i) {
-  if(i >= 0 && i <= a.size) {
-    return a[i];
+int get(array *a, int i) {
+  if(i >= 0 && i <= a->size) {
+    return a->array[i];
   }
+  perror("Failed to get element: Out of range");
+  exit(-1);
 }
 
-void push(array &a, int v) {
+void push(array *a, int v) {
   ensure_capacity(a);
-  a.array[a.size + 1] = v;
+  a->array[a->size] = v;
+  a->size++;
+  return;
 }
 
-int main() {
-  array a = init_array() 
+void pop(array *a) {
+  i(a->size <= 0) {
+    perror("The array is empty to pop element");
+    exit(-1);
+  }
+  else {
+    a->size--;
+  }
+  return;
+}
+
+void free_array(array *a) {
+  free(a->array);
+  a->size = 0;
+  a->capacity = 0;     
+  return;
 }
